76425c2a0b32f5981d99b20c0bd5cebaf7196686
Damon Jones <damonjones@gmail.com>
2018-09-17 18:09:08 -0500

    Update ConsIRAModel

--- HARK/ConsumptionSaving/ConsIRAModel.py
+++ HARK/ConsumptionSaving/ConsIRAModel.py
@@ -26,7 +26,7 @@ sys.path.insert(0, os.path.abspath('./'))
 
 from core import AgentType, NullFunc, HARKobject
 from interpolation import CubicInterp, LowerEnvelope, LinearInterp
-from ConsIndShockModel import ConsumerSolution
+from ConsIndShockModel import ConsumerSolution, ConsKinkedRsolver
 from simulation import drawDiscrete, drawBernoulli, drawLognormal, drawUniform
 from utilities import approxMeanOneLognormal, addDiscreteOutcomeConstantMean,\
                            combineIndepDstns, makeGridExpMult, CRRAutility, CRRAutilityP, \
@@ -55,8 +55,8 @@ class ConsIRASolution(HARKobject):
     distance_criteria = ['cFunc','dFunc']
     
     def __init__(self, cFunc=None, dFunc=None, vFunc=None,
-                       vPfunc=None, mNrmMin=None, nNrmMin=None, 
-                       hNrm=None, MPCmin=None, MPCmax=None):
+                       vPfunc=None, vPPfunc=None, mNrmMin=None, 
+                       nNrmMin=None, hNrm=None, MPCmin=None, MPCmax=None):
         '''
         The constructor for a new ConsumerIRASolution object.
 
@@ -73,6 +73,10 @@ class ConsIRASolution(HARKobject):
             The beginning-of-period marginal value function, with respect to
             m, for this period, defined over liquiud market resources and 
             illiquid account balance: vP = vPfunc(m,n)
+        vPPfunc : function
+            The beginning-of-period marginal marginal value function, with 
+            respect to m, for this period, defined over liquiud market 
+            resources and illiquid account balance: vPP = vPPfunc(m,n)
         mNrmMin : float
             The minimum allowable liquid market resources for this period; 
             the consumption function (etc) are undefined for m < mNrmMin.
@@ -102,17 +106,109 @@ class ConsIRASolution(HARKobject):
             vFunc = NullFunc()
         if vPfunc is None:
             vPfunc = NullFunc()
+        if vPPfunc is None:
+            vPPfunc = NullFunc()
         self.cFunc        = cFunc
         self.dFunc        = dFunc
         self.vFunc        = vFunc
         self.vPfunc       = vPfunc
+        self.vPPfunc      = vPPfunc
         self.mNrmMin      = mNrmMin
         self.nNrmMin      = nNrmMin
         self.hNrm         = hNrm
         self.MPCmin       = MPCmin
         self.MPCmax       = MPCmax
         
-class ConsIRASolver(object):
+class ConsIRASolver(ConsKinkedRsolver):
     '''
+    A class for solving a single period of a consumption-savigs problem with
+    a liquid savings account, and an IRA-like illiquid savings account. Model
+    features constant relative risk aversion utility, permanent and transitory
+    shocks to income, different interest rates for borowing and saving in the
+    liquid account, and a separate interest rate for the illiquid acocunt.
     
+    Inherits from ConsKinkedRsolver, with additional requirements of Rira, with
+    the restriction that Rboro > Rira > Rsave. Also requires the early
+    withdrawal penalty, PenIRA, and a grid for illiquid balance, bXtraGrid.
     '''
+    def __init__(self,solution_next,IncomeDstn,LivPrb,DiscFac,CRRA,
+                     Rboro,Rsave,Rira,PenIRA,PermGroFac,BoroCnstArt,aXtraGrid,
+                     bXtraGrid,vFuncBool,CubicBool):
+        '''
+        Constructor for a new solver for problems with risky income, a liquid
+        and IRA-like illiquid savings account, different interest rates on 
+        liquid borrowing/saving and illiquid saving.
+
+        Parameters
+        ----------
+        solution_next : ConsumerSolution
+            The solution to next period's one period problem.
+        IncomeDstn : [np.array]
+            A list containing three arrays of floats, representing a discrete
+            approximation to the income process between the period being solved
+            and the one immediately following (in solution_next). Order: event
+            probabilities, permanent shocks, transitory shocks.
+        LivPrb : float
+            Survival probability; likelihood of being alive at the beginning of
+            the succeeding period.
+        DiscFac : float
+            Intertemporal discount factor for future utility.
+        CRRA : float
+            Coefficient of relative risk aversion.
+        Rboro: float
+            Interest factor on liquid assets between this period and the 
+            succeeding period when assets are negative.
+        Rsave: float
+            Interest factor on liquid assets between this period and the 
+            succeeding period when assets are positive.
+        Rira:  float
+            Interest factor on illiquid assets between this period and the 
+            succeeding period.
+        PenIRA: float
+            Penalty for early withdrawals (d < 0) from the illiqui account, 
+            i.e. before t = T_ira.
+        PermGroFac : float
+            Expected permanent income growth factor at the end of this period.
+        BoroCnstArt: float or None
+            Borrowing constraint for the minimum allowable assets to end the
+            period with.  If it is less than the natural borrowing constraint,
+            then it is irrelevant; BoroCnstArt=None indicates no artificial 
+            borrowing constraint.
+        aXtraGrid: np.array
+            Array of "extra" end-of-period liquid asset values-- assets above 
+            the absolute minimum acceptable level.
+        bXtraGrid: np.array
+            Array of "extra" end-of-period illiquid asset values-- assets above 
+            the absolute minimum acceptable level.
+        vFuncBool: boolean
+            An indicator for whether the value function should be computed and
+            included in the reported solution.
+        CubicBool: boolean
+            An indicator for whether the solver should use cubic or linear inter-
+            polation.
+
+        Returns
+        -------
+        None
+        '''
+        assert Rboro>=Rira>=Rsave, 'Interest factors must satisfy Rboro>=Rira>=Rsave'
+        
+        # Set notation
+        self.notation = {'a': 'liquid assets after all actions',
+                         'b': 'illiquid assets after all actions',
+                         'm': 'liquid market resources at decision time',
+                         'n': 'illiduid market resources at decisiont time',
+                         'c': 'consumption',
+                         'd': 'illiquid deposit'}
+        
+        # Initialize the solver.  Most of the steps are exactly the same as in
+        # kinked-R basic case, so start with that.
+        ConsKinkedRsolver.__init__(self,solution_next,IncomeDstn,LivPrb,
+                                   DiscFac,CRRA,Rboro,Rsave,PermGroFac,
+                                   BoroCnstArt,aXtraGrid,vFuncBool,CubicBool)
+        
+        # Assign illiquid interest rate, illiquid asset grid, and IRA penalty.
+        self.Rira         = Rira
+        self.PenIRA       = PenIRA
+        self.bXtraGrid    = bXtraGrid
+    
